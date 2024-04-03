15b6c83b65bc1b88ab0ca9388db306718bcf2cda
arriel <arrieldongo@gmail.com>
2024-04-03 04:12:23 +0000

    toujours en progression sur la formation avec le design system part01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,27 @@ import { Seo } from "@/ui/component/seo/seo";
 import { Typographie } from "@/ui/design-system/typographie/typographie";
 import { Inter } from "next/font/google";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return ( <>
+
     < Seo title="first app react" description="content"/>
-    <Typographie variant="h1">
-      coder Monkeys
-    </Typographie>
+
+    <div className="space-y-5">
+      <Typographie variant="display" component="div"> coder Monkeys </Typographie>
+      <Typographie theme="primary" variant="h1" component="div"> coder Monkeys </Typographie>
+      <Typographie theme="secondary" variant="h4" component="div"> coder Monkeys </Typographie>
+      <Typographie variant="h5" component="div"> coder Monkeys </Typographie>
+      <Typographie variant="lead" component="div"> coder Monkeys </Typographie>
+      <Typographie variant="Body-sm" component="div"> coder Monkeys </Typographie>
+      <Typographie variant="Body-lg" component="div"> coder Monkeys </Typographie>
+      <Typographie variant="caption-4" component="div"> coder Monkeys </Typographie>
+      <Typographie variant="caption-4" weight="medium" component="div"> coder Monkeys </Typographie>
+
+    </div>
+    
      
   
   </>
--- src/ui/design-system/typographie/typographie.tsx
+++ src/ui/design-system/typographie/typographie.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import 'tailwindcss/tailwind.css';
+
 
 
 interface props{
@@ -18,20 +21,134 @@ interface props{
     | "caption-4";
   
   component?: 
-  | "h1" 
-  | "h2" 
-  | "h3" 
-  | "h4" 
-  | "h5" 
-  | "div" 
-  | "p" 
-  | "span";
-  
+    | "h1" 
+    | "h2" 
+    | "h3" 
+    | "h4" 
+    | "h5" 
+    | "div" 
+    | "p" 
+    | "span";
+
+  theme?: "black" | "gray" | "white" | "primary" | "secondary";
+  weight?: "regular" | "medium";
+  className?: string
   children: React.ReactNode;
 }
 
-export const Typographie = ({variant, component, children}:props) => {
- return <>
+export const Typographie = ({
+  variant = "h3",
+   component: Component = "div",
+   theme = "black", 
+   weight = "regular",
+   className,
+   children
+  
+  
+  }:props) => {
+
+  let variantStyles: string = "", colorStyles: string = "";
+
+  switch (variant) {
+
+    case "display":
+      variantStyles = "text-8xl";
+      break;
+
+    case "h1":
+      variantStyles = "text-7xl";
+      break;
+
+    case "h2":
+      variantStyles = "text-6xl";
+      break;
+
+    case "h3": // default
+      variantStyles = "text-5xl";
+      break;
+
+    case "h4":
+      variantStyles = "text-4xl";
+      break;
+
+    case "h5":
+      variantStyles = "text-3xl";
+      break;
+
+    case "lead":
+      variantStyles = "text-2xl";
+      break;
+
+    case "Body-lg":
+      variantStyles = "text-lg";
+      break;
+
+    case "Body-base":
+      variantStyles = "text-base";
+      break;
+    
+    case "Body-sm":
+      variantStyles = "text-sm";
+      break;
+
+    case "caption-1":
+      variantStyles = "text-caption_1";
+      break;
+
+    case "caption-2":
+      variantStyles = "text-caption_2";
+      break;
+
+    case "caption-3":
+      variantStyles = "text-caption_3";
+      break;
+
+    case "caption-4":
+      variantStyles = "text-caption_4";
+      break;
+  }
+
+  switch (theme) {
+
+    case "black": //default
+      colorStyles =  "text-gray";
+      break;
+
+    case "gray":
+      colorStyles =  "text-gray-700";
+      break;
+
+    case "white":
+      colorStyles =  "text-white";
+      break;
+
+    case "primary":
+      colorStyles =  "text-primary";
+      break;
+
+    case "secondary":
+      colorStyles =  "text-secondary";
+      break;
+  
+  default:
+    break;
+
+  }
+
+
+
+ return <Component 
+ className= {
+  clsx(
+    variantStyles,
+    colorStyles,
+    weight === "medium" && "font-medium",
+    className,
+  
+  )} >
+
+
   {children}
- </>
+
+ </Component>
 }
